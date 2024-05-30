a2d4a4885b0f040ebdae256f8621f76c82a200f0
unknown <bexruzraxmanov59@gmail.com>
2024-05-30 13:19:19 +0500

    gotovo

--- main.py
+++ main.py
@@ -1,32 +1,51 @@
-name = input("Как тебя звать матиматик ? : ")
+name = input("Как вас зовут ? : ")
 ball = 0
 from random import randrange
 def rand():
-    num1 = randrange(1,100)
-    num2 = randrange(1,100)
-    deystvie = randrange(0,4)
+    global ball
+    num1 = randrange(1,10)
+    num2 = randrange(1,10)
+    deystvie = randrange(0,2)
     otv = 0
-    if deystvie == 0:
-        deystvie = "-"
-        otv = num1 - num2
-    elif deystvie == 1:
-        deystvie = "+"
-        otv = num1 + num2
-    elif deystvie == 2:
-        deystvie = "/"
-        otv = num1 / num2
-    elif deystvie == 3:
-        deystvie = "*"
-        otv = num1 * num2
-    user_otv = int(input(f"{num1} {deystvie} {num2} = "))
-    if user_otv == otv:
-        print("Правильно!")
-        global ball
-        ball += 1
-        print(f"Правильных ответов  {ball}")
-        rand()
-    elif user_otv != otv:
-        print(f"{name} Ты что совсем тупой?!\n Твои баллы сбрасываються ")
-        ball = 0
-        rand()
-rand()
+    if ball >= 3:
+        print("Поздравляю вы победили!")
+    else:
+        if deystvie == 0:
+            deystvie = "-"
+            otv = num1 - num2
+        elif deystvie == 1:
+            deystvie = "+"
+            otv = num1 + num2
+        # для повышения уровня сложности
+        # elif deystvie == 2:
+        #     deystvie = "/"
+        #     otv = num1 / num2
+        # elif deystvie == 3:
+        #     deystvie = "*"
+        #     otv = num1 * num2
+        try:
+            user_otv = float(input(f"{num1} {deystvie} {num2} = "))
+        except:
+            print("Пожалуста пишите ответ цифрами \nВаши баллы сбрасываються ")
+            rand()
+        if user_otv == otv:
+            print("Правильно продалжайте в том же духе! ")
+            ball += 1
+            print(f"Правильных ответов  {ball}")
+            rand()
+        elif user_otv != otv:
+            print(f"{name} Увы но ваш ответ не правильный \nВаши баллы сбрасываються ")
+            ball = 0
+            rand()
+        
+vopr = input('хотите ли вы поучаствовать в математической викторине : ')
+vopr = vopr.lower().replace(" ","")
+if vopr == "да":
+    print("Что бы победить ответить на 3 вопроса подрять")
+    rand()
+elif vopr == "нет":
+    print("Ну ладно")
+else:
+    print("Не понимаю но приму как 'да'")
+    print("Что бы победить ответить на 3 вопроса подрять")
+    rand()
